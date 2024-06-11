c4b0e764aac7cc0e317058815fe9c7a078fe8db5
MoHaydar <168010559+MoHaydar@users.noreply.github.com>
2024-06-11 14:48:20 +0300

    Update displayorder.php

--- displayorder.php
+++ displayorder.php
@@ -22,7 +22,7 @@ $successMessage="";
 if(($_SERVER['REQUEST_METHOD']) == 'GET'){
 
     if(!isset($_GET['id'])){
-        header("location:ad-min.php");
+        header("location:/Adminpage.php/index1.php");
         exit;
     }
     $id=$_GET["id"];
@@ -31,7 +31,7 @@ if(($_SERVER['REQUEST_METHOD']) == 'GET'){
     $row = $result-> fetch_assoc();
 
     if (!$row){
-        header("location :/Adminpage.php/index.php");
+        header("location :/Adminpage.php/index1.php");
         exit;
     }
 }
